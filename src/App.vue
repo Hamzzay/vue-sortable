e49957ecaf62dd62760cb830dee57ebc5dfc943a
Hamzzay <hamzzayyyy@gmail.com>
2023-11-28 17:42:39 +0500

    sortable functionality completed with localstorage

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <SortableDragDrop />
+</template>
+
+<script>
+
+import SortableDragDrop from "./components/SortableDragDrop.vue";
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+  components: {
+    SortableDragDrop
+  }
+}
+
+</script>
+
